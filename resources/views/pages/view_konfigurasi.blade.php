724e541e5cafa0d41b1d225e3b64d190420760b2
silvranz <silvranz@gmail.com>
2016-03-10 01:17:28 +0700

    tambahan

--- resources/views/pages/view_konfigurasi.blade.php
+++ resources/views/pages/view_konfigurasi.blade.php
@@ -15,9 +15,6 @@
 </div>
 <div class="form-group schema">
 	<div class="alert alert-danger hide" id="warningMessage"></div>
-	<svg width="100%" height="100%" preserveAspectRatio="none" id="relationContainer">
-		Sorry, your browser does not support inline SVG.
-	</svg><br>
 	<div class="form-inline">
 		<div class="form-group" style="padding-right:10px;">
 			<button type="button" class="btn btn-default" onclick="location.href='{{ URL::to('admin/konfigurasi/edit/0') }}';">Add</button>
@@ -32,5 +29,8 @@
 			<button type="button" class="btn btn-default" onclick="location.href='{{ URL::to('admin/konfigurasi/simulate') }}';">Simulate</button>
 		</div>
 	</div><br><br>
+	<svg width="100%" height="100%" preserveAspectRatio="none" id="relationContainer">
+		Sorry, your browser does not support inline SVG.
+	</svg><br>
 </div>
 @stop
